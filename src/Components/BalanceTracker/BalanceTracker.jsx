0711a3c24054fc9b794dd3902c28d58810e0ee31
Abhay Dixit <abhaydt.08@gmail.com>
2024-03-26 13:33:47 +0530

    Final design for the Balance Tracker ready

--- src/Components/BalanceTracker/BalanceTracker.jsx
+++ src/Components/BalanceTracker/BalanceTracker.jsx
@@ -13,7 +13,7 @@ const BalanceTracker = () => {
     { name: "Travel", value: 50 },
   ];
 
-  const COLORS = ['#FF9304', '#A000FF', '#FDE006'];
+  const COLORS = ["#FF9304", "#A000FF", "#FDE006"];
 
   const RADIAN = Math.PI / 180;
   const renderCustomizedLabel = ({
@@ -44,35 +44,58 @@ const BalanceTracker = () => {
 
   return (
     <div className={styles.balanceTracker}>
-      <div className={styles.walletBalance}>
-        <h3 className={styles.heading}>
-          Wallet Balance:{" "}
-          <span className={styles.balance}>₹{walletBalance}</span>
-        </h3>
-        <button className={styles.addIncomeBtn}>+ Add Income</button>
+      <div className={styles.balanceContainer}>
+        <div className={styles.walletBalance}>
+          <h3 className={styles.heading}>
+            Wallet Balance:{" "}
+            <span className={styles.balance}>₹{walletBalance}</span>
+          </h3>
+          <button className={styles.addIncomeBtn}>+ Add Income</button>
+        </div>
+        <div className={styles.expensesContainer}>
+          <h3 className={styles.heading}>
+            Expenses: <span className={styles.expenses}>₹{expenses}</span>
+          </h3>
+          <button className={styles.addExpenseBtn}>+ Add Expense</button>
+        </div>
       </div>
-      <div className={styles.expensesContainer}>
-        <h3 className={styles.heading}>
-          Expenses: <span className={styles.expenses}>₹{expenses}</span>
-        </h3>
-        <button className={styles.addExpenseBtn}>+ Add Expense</button>
+      <div className={styles.balanceChart}>
+        <PieChart width={200} height={200}>
+          <Pie
+            data={data}
+            cx="50%"
+            cy="50%"
+            labelLine={false}
+            label={renderCustomizedLabel}
+            outerRadius={100}
+            fill="#626262"
+            stroke="none"
+            dataKey="value"
+            activeShape={null}
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+        <div className={styles.indicatorsContainer}>
+          <div className={styles.indicator}>
+            <span className={styles.indicator1Color}></span>
+            <div className={styles.indicatorName}>Food</div>
+          </div>
+          <div className={styles.indicator}>
+            <span className={styles.indicator2Color}></span>
+            <div className={styles.indicatorName}>Entertainment</div>
+          </div>
+          <div className={styles.indicator}>
+            <span className={styles.indicator3Color}></span>
+            <div className={styles.indicatorName}>Travel</div>
+          </div>
+        </div>
       </div>
-      <PieChart width={200} height={200}>
-        <Pie
-          data={data}
-          cx="50%"
-          cy="50%"
-          labelLine={false}
-          label={renderCustomizedLabel}
-          outerRadius={80}
-          fill="#8884d8"
-          dataKey="value"
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ))}
-        </Pie>
-      </PieChart>
     </div>
   );
 };
